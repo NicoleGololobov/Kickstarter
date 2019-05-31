da69837d960762d32aec030eb7712cea7f788301
Nicole Gololobov <48893008+NicoleGololobov@users.noreply.github.com>
2019-05-31 13:53:23 -0400

    Update Kick.py
    Remove part of dataframe

--- Kick.py
+++ Kick.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 # Read data into pandas dataframe
 kick_data = pd.read_csv("Kickstarter002.csv")
+# Isolate relevant attributes
+kick_data = kick_data.loc[:, ['backers_count','blurb','category','country', 'goal', 'staff_pick', 'state', 'usd_pledged']]
 
 # Construct seaborn boxplot to see how being a staff pick affects how much is pledged
 sns.boxplot(x="staff_pick", y="usd_pledged", data=kick_data)
