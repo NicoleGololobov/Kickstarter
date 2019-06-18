d0f7cf0e929c9a0a45934cec5437042d064d9e9f
Nicole Gololobov <48893008+NicoleGololobov@users.noreply.github.com>
2019-06-18 12:46:52 -0400

    Update file
    Divide data into training and test sets

--- Kick.R
+++ Kick.R
@@ -7,3 +7,8 @@ kick_data = kick_data[!(kick_data$state=="live"),]
 kick_data$category<-as.factor(kick_data$category)
 kick_data$country<-as.factor(kick_data$country)
 
+# 75% of rows for training data
+samp_size = floor(0.75 * nrow(kick_data))
+samp = sample(seq_len(nrow(kick_data)), size = samp_size)
+train = kick_data[samp,]
+test = kick_data[-samp, ]
